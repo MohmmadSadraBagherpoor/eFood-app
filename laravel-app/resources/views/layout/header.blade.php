6d11f2b436736c8df94287bfbba8dde48a16d6bb
MohmmadSadraBagherpoor <bagherpoorsadra@gmail.com>
2025-04-08 11:52:52 +0330

    dynamic slider on laravel-app

--- laravel-app/resources/views/layout/header.blade.php
+++ laravel-app/resources/views/layout/header.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>webprog.io || @yield('title')</title>
-    
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.rtl.min.css"
         integrity="sha384-+qdLaIRZfNu4cVPK/PxJJEy0B0f3Ugv8i482AKY7gwXwhaCroABd086ybrVKTa0q" crossorigin="anonymous">
 
@@ -21,7 +21,7 @@
 
 </head>
 <body>
-    <div>
+    <div class="{{ request()->is('/') ? '' : 'sub_page' }}">
         <div class="hero_area">
             <div class="bg-box">
                 <img src="{{ asset('/images/hero-bg.jpg') }}" alt="">
@@ -73,103 +73,8 @@
                 </div>
             </header>
             <!-- end header section -->
-            <!-- slider section -->
-            <section class="slider_section">
-                <div id="customCarousel1" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-md-7 col-lg-6">
-                                        <div class="detail-box">
-                                            <h2 class="mb-3 fw-bold">
-                                                لورم ایپسوم متن 
-                                            </h2>
-                                            <p>
-                                                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
-                                                از
-                                                طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و
-                                                سطرآنچنان که
-                                                لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف
-                                                بهبود
-                                                ابزارهای کاربردی می باشد.
-                                            </p>
-                                            <div class="btn-box">
-                                                <a href="" class="btn1">
-                                                    سفارش
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-md-7 col-lg-6">
-                                        <div class="detail-box">
-                                            <h2 class="mb-3 fw-bold">
-                                                لورم ایپسوم متن ساختگی
-                                            </h2>
-                                            <p>
-                                                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
-                                                از
-                                                طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و
-                                                سطرآنچنان که
-                                                لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف
-                                                بهبود
-                                                ابزارهای کاربردی می باشد.
-                                            </p>
-                                            <div class="btn-box">
-                                                <a href="" class="btn1">
-                                                    سفارش
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-md-7 col-lg-6">
-                                        <div class="detail-box">
-                                            <h2 class="mb-3 fw-bold">
-                                                لورم ایپسوم متن ساختگی
-                                            </h2>
-                                            <p>
-                                                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
-                                                از
-                                                طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و
-                                                سطرآنچنان که
-                                                لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف
-                                                بهبود
-                                                ابزارهای کاربردی می باشد.
-                                            </p>
-                                            <div class="btn-box">
-                                                <a href="" class="btn1">
-                                                    سفارش
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="container">
-                        <ol class="carousel-indicators">
-                            <li data-bs-target="#customCarousel1" data-bs-slide-to="0" class="active"></li>
-                            <li data-bs-target="#customCarousel1" data-bs-slide-to="1"></li>
-                            <li data-bs-target="#customCarousel1" data-bs-slide-to="2"></li>
-                        </ol>
-                    </div>
-                </div>
-
-            </section>
-
-            <!-- end slider section -->
+            @if(request()->is('/'))
+                @include('home.slider')
+            @endif
         </div>
     </div>
--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -4,4 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home.index');
-});
+})->name('home.index');
+
+Route::get('/about', function () {
+    return view('about.index');
+})->name('about.index');
